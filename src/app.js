c0ec7df213a22f101cef3b0801b41a8798a44741
santiagoGuastavino <san.guasta@gmail.com>
2021-09-13 21:03:50 -0300

    react build loads alright

--- src/app.js
+++ src/app.js
@@ -1,60 +1,10 @@
-// let express = require('express');
-// let app = express();
-// let path = require('path');
-// -------------------------------------------------------------------------- //
-// ROUTES
-// REQUIRE ROUTE
-// let requiredRouter = ('path to router odule')
-// USE ROUTES
-// app.use('/route',requiredRouter);
-// -------------------------------------------------------------------------- //
-// GLOBAL MIDDLEWARES
-// app.use('requiredMiddleware'); //ex: logs
-// -------------------------------------------------------------------------- //
-// SET PUBLIC
-// app.use(express.static('public'))
-// -------------------------------------------------------------------------- //
-// EJS TEMPLATE ENJINE
-// npm i ejs
-// app.set('view engine', 'ejs')
-// app.set('views', viewsPath)
-// -------------------------------------------------------------------------- //
-// ERROR
-// app.use((req,res,next) => {
-//  res.status(404).render('not-found-view.ejs', { status: 404 } );
-//  next();
-// })
-// -------------------------------------------------------------------------- //
-// PUT & DELETE w/ METHOD OVERRIDE
-// npm i method-override
-// let methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
-// IN THE VIEW
-// <form action="/route?_method=PUT/DELETE" method="POST"></form>
-// -------------------------------------------------------------------------- //
-// POST
-// to parse the body
-// app.use(express.urlencoded ({extended:false}));
-// app.use(express.json());
-// -------------------------------------------------------------------------- //
-// SESSION
-// npm i express-session
-// let session = require('express-session')
-// GLOBAL MIDDLEWARE:
-// app.use(session({ 
-//      secret: 'session-secret',
-//      resave: false,
-//      saveUninitialized: true
-// }));
-// -------------------------------------------------------------------------- //
-// EXPRESS VALIDATOR
-// npm i express-validator
-// -------------------------------------------------------------------------- //
-// COOKIE
-// npm i cookie-parser
-// let cookies = require('cookie-parser');
-// GLOBAL MIDDLEWARE:
-// app.use(cokies());
-// -------------------------------------------------------------------------- //
-// SERVER LISTENING
-// app.listen(3001)
+let express = require('express');
+let app = express();
+let path = require('path');
+
+let buildPath = path.resolve('./public/build');
+
+app.use(express.static(buildPath));
+
+let port = process.env.PORT || 3001;
+app.listen(port, () => console.log('Server running: 3001'));
