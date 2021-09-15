2205a527ebc0c8b0583ba362368d9b4fcfc89e3e
santiagoGuastavino <san.guasta@gmail.com>
2021-09-15 13:19:40 -0300

    fixed path

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,9 @@
 let path = require('path');
-let indexPath = path.resolve('./public/build/index.html');
+// let indexPath = path.resolve('./public/build/index.html');
 
 let mainController = {
     index: (req, res) => {
-        res.sendFile('indexPath');
+        res.sendFile(path.resolve('./public/build/index.html'));
     }
 };
 
